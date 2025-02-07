94bd8520e44cc9191234b65c7b8d50dfef3963ab
Maurosacramento <maurosacramento35@gmail.com>
2025-02-07 12:32:59 +0000

    Create profile page

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IconsModule } from '../../shared/icons/icons.module';
+
+@Component({
+  selector: 'app-profile',
+  imports: [IconsModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+
+}

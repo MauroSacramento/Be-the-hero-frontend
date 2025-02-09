eb45884bcf569afdb2595a7d1ed707b02feb9daa
Maurosacramento <maurosacramento35@gmail.com>
2025-02-09 21:00:20 +0000

    Validating forms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, inject } from '@angular/core';
 import { IconsModule } from '../../shared/icons/icons.module';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [IconsModule, RouterLink],
+  imports: [IconsModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
   private route = inject(Router);
+
+  LoginForm = new FormGroup({
+    id: new FormControl('', {validators: [Validators.required]})
+  });
+
+  onSubmit(){
+    if(this.LoginForm.invalid){
+      return
+    }
+    const id = this.LoginForm.controls.id;
+    this.route.navigate(['/profile'])
+  }
 
 }

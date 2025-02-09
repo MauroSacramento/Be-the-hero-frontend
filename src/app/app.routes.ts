f1fb8f3e3c97defc9e45eb67ce2a61667bec67b6
Maurosacramento <maurosacramento35@gmail.com>
2025-02-09 12:22:05 +0000

    create new-case component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { NewCaseComponent } from './components/new-case/new-case.component';
 
 export const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: ProfileComponent}
+  {path: 'profile', component: ProfileComponent},
+  {path: 'new', component: NewCaseComponent}
 ];
---
+++ src/app/components/new-case/new-case.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IconsModule } from '../../shared/icons/icons.module';
+
+@Component({
+  selector: 'app-new-case',
+  imports: [IconsModule],
+  templateUrl: './new-case.component.html',
+  styleUrl: './new-case.component.css'
+})
+export class NewCaseComponent {
+
+}
